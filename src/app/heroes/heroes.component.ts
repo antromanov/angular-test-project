e2ff051dc6345753fdc2e8a058443b120d8ed25c
Anton Romanov <romanov.exceedteam@yandex.ru>
2020-11-25 17:27:55 +0300

    Added routes and details component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
 import { HeroService } from '../hero.service'
-import { MessageService } from '../message.service'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroes?: Hero[]
+  heroes!: Hero[]
 
-  selectedHero: Hero | undefined
-
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes()
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
